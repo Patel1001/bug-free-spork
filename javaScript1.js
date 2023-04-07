d8f892e1df0b06ef0fd1dda0657a4e34865ab07e
Patel1001 <130114311+Patel1001@users.noreply.github.com>
2023-04-06 22:12:31 -0300

    Add files via upload
    initial upload

---
+++ javaScript1.js
@@ -0,0 +1,85 @@
+
+
+if ( window.location.pathname === '/Users/darshpatel/Documents/A3_DR892704/index.html') {
+  window.addEventListener('load', function() {
+    alert('Welcome to my website!');
+  });
+}
+
+const menuBar = document.querySelector('nav.menu');
+// function setButtonColors() {
+    // Normal mode button
+    const normalModeButton = document.getElementById("normal-mode");
+    normalModeButton.addEventListener("click", function() {
+      document.body.style.backgroundColor = "white";
+      document.body.style.color = "black";
+      document.querySelectorAll('p').forEach(function(p) {
+        p.style.backgroundColor = "#edf9fa";
+      });
+      menuBar.style.backgroundColor = "#277284";
+    });
+  
+    // Dark mode button
+    const darkModeButton = document.getElementById("dark-mode");
+    darkModeButton.addEventListener("click", function() {
+      document.body.style.backgroundColor = "black";
+      document.body.style.color = "orange";
+      document.querySelectorAll('p').forEach(function(p) {
+        p.style.backgroundColor = "transparent";
+      });
+      menuBar.style.backgroundColor = "black";
+    
+    });
+  
+    // High contrast mode button
+    const highContrastModeButton = document.getElementById("high-contrast-mode");
+    highContrastModeButton.addEventListener("click", function() {
+      document.body.style.backgroundColor = "yellow";
+      document.body.style.color = "black";
+      document.querySelectorAll('p').forEach(function(p) {
+        p.style.backgroundColor = "transparent";
+      });
+      menuBar.style.backgroundColor = "black";
+    });
+  
+  
+    const hidePhotosButton = document.getElementById('hidePhotosButton');
+    const gallery = document.querySelector('.gallery');
+    
+    hidePhotosButton.addEventListener('click', function() {
+      const confirmHide = confirm('Are you sure you want to hide the photos?');
+      
+      if (confirmHide) {
+        gallery.style.display = 'none';
+      }
+    });
+
+    const timeButton = document.getElementById('time-button');
+    const timeDisplay = document.getElementById('time-display');
+
+  function updateTimeDisplay() {
+  const now = new Date();
+  const timeString = now.toLocaleTimeString();
+  timeDisplay.textContent = `The current time is ${timeString}.`;
+}
+
+timeButton.addEventListener('click', updateTimeDisplay);
+
+
+/*Reference taken from W3Schools*/
+
+  const images = ["/Users/darshpatel/Documents/A3_DR892704/Images/skywallpaper.jpg", "/Users/darshpatel/Documents/A3_DR892704/Images/Sunrise.jpeg" ];
+  let currentImageIndex = 0;
+  const imageElement = document.getElementById("myImage");
+
+  function changeImage() {
+    currentImageIndex++;
+    if (currentImageIndex >= images.length) {
+      currentImageIndex = 0;
+    }
+    imageElement.src = images[currentImageIndex];
+  }
+
+  setInterval(changeImage, 5000); // Change image every 5 seconds
+
+
